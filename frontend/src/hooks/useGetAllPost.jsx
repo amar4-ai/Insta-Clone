3536d12592aa01e8e2d494e1409ed2d06da496bc
Amar <amarkhadkabardiya1234@gmail.com>
2026-03-04 18:32:22 +0545

    URL changes

--- frontend/src/hooks/useGetAllPost.jsx
+++ frontend/src/hooks/useGetAllPost.jsx
@@ -7,7 +7,7 @@ const useGetAllPost = ()=>{
     useEffect(()=>{
         const fetchAllPost = async()=>{
             try {
-                const res= await axios.get('http://localhost:8000/api/v1/post/all', {withCredentials :true});
+                const res= await axios.get('https://insta-clone-5she.onrender.com/api/v1/post/all', {withCredentials :true});
                 if(res.data.success){
                     // console.log(res.data);
                     dispatch(setPosts(res.data.posts));
