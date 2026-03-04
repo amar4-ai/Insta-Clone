ed1d5c0ca6b449909bf290c5648e31e8baabee71
Amar <amarkhadkabardiya1234@gmail.com>
2026-03-04 18:09:30 +0545

    changes for deploy

--- backend/index.js
+++ backend/index.js
@@ -3,46 +3,40 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-import userRoute from  "./routes/user.route.js";
+import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
-import messageRoute from "./routes/message.route.js"
+import messageRoute from "./routes/message.route.js";
 import { app, server } from "./socket/socket.js";
 import path from "path";
-dotenv.config({});
 
+dotenv.config({});
 
 const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
-
-app.get("/",(req,res)=>{
-    return res.status(200).json({
-        message:"I am coming from backend",
-        success:true
-    })
-})
-
-//middleware
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(urlencoded({extended:true}));
-const corsOptions ={
-    origin:"http://localhost:5173",
-    credentials:true
-}
+app.use(urlencoded({ extended: true }));
+
+const corsOptions = {
+    origin: process.env.URL || "http://localhost:5173",
+    credentials: true
+};
 app.use(cors(corsOptions));
-//api
+
+// API routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
 app.use("/api/v1/message", messageRoute);
 
+// Serve frontend
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
-app.get("*splat",(req,res)=>{
+app.get("*splat", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-})
+});
 
-
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     connectDB();
     console.log(`Server listen at port ${PORT}`);
-})
+});
